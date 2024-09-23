5b8aec740660a6eb80d3d3523d249d1d2b49e43b
Lucas Bubner <lkbubner@proton.me>
2024-09-23 16:43:54 +0930

    egcd

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
 public class Main {
+    public static int[] extendedGcd(int a, int b) {
+        if (b == 0) {
+            // Base case, gcd(a,b) = ax + by = a*1 + b*0 = a
+            return new int[]{a, 1, 0};
+        }
+        // Recursively find the GCD by the modulo property
+        int[] ret = extendedGcd(b, a % b);
+        // Calculate the new x and y used to calculate the GCD
+        int tmp = ret[1] - ret[2] * (a / b);
+        ret[1] = ret[2];
+        ret[2] = tmp;
+        return ret;
+    }
+
     public static void main(String[] args) {
     }
 }
